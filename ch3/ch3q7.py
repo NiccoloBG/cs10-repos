226ab83fa721b582434c1d85fe5c78ea5f9f43d6
NiccoloBG <115162455+NiccoloBG@users.noreply.github.com>
2022-11-02 09:44:17 -0400

    Add files via upload

---
+++ ch3/ch3q7.py
@@ -0,0 +1,17 @@
+import random
+import turtle as tl
+
+screen = tl.Screen()
+pen = tl.Turtle()
+
+testset = [160, -43, 270, -97, -43, 200, -940, 17, -86]
+
+for i in testset:
+    pen.forward(100) #with the assumption units = steps
+    pen.right(i)
+
+for i in range(int(input('How many more steps forward do you want to predict?'))):
+    pen.forward(100)
+    pen.right(random.randint(-360, 360))
+
+screen.mainloop()
